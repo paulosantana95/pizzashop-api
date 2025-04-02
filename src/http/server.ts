4b45b76ed9f2ba059a061b4742a3a57b274d1779
Paulo Santana <paulosantana.sa@gmail.com>
2025-04-01 23:53:50 -0300

    feat: add authentication link handling and JWT integration

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,25 +1,14 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 import chalk from 'chalk'
-import jwt from '@elysiajs/jwt'
-import { env } from '../env'
-import cookie from '@elysiajs/cookie'
 
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+import { authenticateFromLink } from './routes/authenticate-from-link'
 
 const app = new Elysia()
-  .use(
-    jwt({
-      secret: env.JWT_SECRET_KEY,
-      schema: t.Object({
-        sub: t.String(),
-        restaurantId: t.Optional(t.String()),
-      }),
-    }),
-  )
-  .use(cookie())
   .use(registerRestaurant)
   .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log(chalk.blueBright('🔥 HTTP server running'))
